59656f721a9e68301d2f6e4e9940749729772df1
Robert Prehn <robert@revelry.co>
2015-05-16 07:48:02 -0500

    ignore migrations in rubocop

--- app/models/linters/rubo_cop.rb
+++ app/models/linters/rubo_cop.rb
@@ -1,10 +1,19 @@
 class Linters::RuboCop < Linters::Base
   def run(file)
-    return [] if file.path == 'db/schema.rb'
+    return [] if ignored_file?(file)
     runner = RuboCop::Runner.new({}, RuboCop::ConfigStore.new)
     processed_source = processed_source_for(file)
     offenses, _ = runner.send(:inspect_file, processed_source)
     offenses.map { |o| Violation.new(file: file, line: o.location.line, message: o.message) }
+  end
+
+  def ignored_file?(file)
+    case file.path
+    when *['db/schema.rb', %r{^db/migrate}]
+      true
+    else
+      false
+    end
   end
 
   def processed_source_for(file)
